69e17e8ebdc9fd85e77dbace746d1ce035ded7e4
Ashly LaMarr <surlymae@gmail.com>
2023-12-12 10:23:09 -0700

    mvp

---
+++ chat.js
@@ -0,0 +1,47 @@
+import "dotenv/config";
+import readline from "node:readline";
+import { openAI } from "./openai.js";
+
+const readLine = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const newMessage = async (history, message) => {
+  const chatCompletion = await openAI.chat.completions.create({
+    messages: [...history, message],
+    model: "gpt-3.5-turbo",
+  });
+
+  return chatCompletion.choices[0].message;
+};
+
+const formatMessage = (userInput) => ({ role: "user", content: userInput });
+
+const chat = () => {
+  const history = [
+    {
+      role: "system",
+      content: "you are a cynical software engineer. Answer questions please.",
+    },
+  ];
+  const start = () => {
+    readLine.question("you: ", async (userInput) => {
+      if (userInput.toLowerCase() === "exit") {
+        readLine.close();
+        return;
+      }
+
+      const userMessage = formatMessage(userInput);
+      const response = await newMessage(history, userMessage);
+
+      history.push(userMessage, response);
+      console.log(`\n\nai: ${response.content}\n\n`);
+      start();
+    });
+  };
+  start();
+};
+
+console.log("chat initialized; type exit to end");
+chat();
